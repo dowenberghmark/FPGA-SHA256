a758d9640bfe43bc335f44d03b9a68ff56b5e7f9
jeffjonasson <jeff.jonasson@gmail.com>
2017-11-14 14:57:52 +0100

    Added final padding

--- cpu/application.py
+++ cpu/application.py
@@ -7,14 +7,12 @@ import sys
 #translates ascii strings to binary strings
 #input: textfile
 #return: list of passwords in binary string
-def ascii_to_bits(text,encoding='utf-8', errors='surrogatepass'):
-	binary_passwords = []
-	with open(text,'rw+') as un_hashed_passwords:
-		passwords = un_hashed_passwords.readlines()
+def ascii_to_bits(passwords,encoding='utf-8', errors='surrogatepass'):
 
-		for line in passwords:
-			bits = bin(int(binascii.hexlify(line.encode(encoding, errors)), 16))[2:]
-			binary_passwords.append(bits.zfill(8 * ((len(bits) + 7) // 8)))
+	binary_passwords = []
+	for line in passwords:
+		bits = bin(int(binascii.hexlify(line.encode(encoding, errors)), 16))[2:]
+		binary_passwords.append(bits.zfill(8 * ((len(bits) + 7) // 8)))
 
 	print 'this is binary passwords: ', type(binary_passwords)
 	return binary_passwords
@@ -29,24 +27,35 @@ def append_one(params):
 def append_k(params):
 	k = []
 	for i in range(0,len(params)):
-		byte_size = bytearray(params[i])
-		size_of_byte_size = sys.getsizeof(byte_size)
-		k.append(448%512 - size_of_byte_size)
+		k.append(448%512 - len(params[i]))
 
 	for i in range(0,len(params)):
 		params[i] = params[i] + (k[i]*'0')
 
+
 	return params
+
+def append_64(params, passwords):
+
+	for i in range (0,len(params)):
+		params[i] = params[i]+ '{0:064b}'.format((8*len(passwords[i])))
+
+	return params
+
 def prepare_passwords():
+	with open('password.txt', 'rw+') as un_hashed_passwords:
+		passwords = un_hashed_passwords.readlines()
 	
-	bin_array = ascii_to_bits('password.txt')
+	bin_array = ascii_to_bits(passwords)
 	add_1 = append_one(bin_array)
 	print add_1
-	append_k(add_1)
-def main():
+	add_2 = append_k(add_1)
+	append_64(add_2, passwords)
 
+
+def main():
 
 	prepare_passwords()
 if __name__ == "__main__":
-    main()
+	main()
 
