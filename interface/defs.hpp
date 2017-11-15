203a56203ddac1fc522bd2f2b3e335cce6dbd314
Adam Wendelin <Adam.Wendelin.1858@student.uu.se>
2017-11-15 14:06:00 +0100

    defs for buffer

---
+++ interface/defs.hpp
@@ -0,0 +1,24 @@
+#ifndef __DEFS_H_INCLUDED__
+#define __DEFS_H_INCLUDED__
+
+
+struct chunk {
+	char data[64];
+};
+
+struct buffer_header {
+	uint32_t num_chunks;
+	uint32_t ready_flag;
+};
+
+struct buffer {
+	struct buffer_header buffer_header;
+	struct chunk *chunks;
+};
+
+struct global_header {
+	uint32_t start_processing_flag;
+	uint32_t active_buffer_flag;
+};
+
+#endif
