5a569a5956bdf714408834de62b9d3db18395f4c
Daniel <danielowen_b@hotmail.com>
2017-11-15 10:24:25 +0100

    function skelett

--- interface/double_buffer.cpp
+++ interface/double_buffer.cpp
@@ -1,7 +1,13 @@
+#include <mutex>
+
+
 class Double_buffer
 {
+ private:
+  std::mutex first_buffer;
+  std::mutex second_buffer;
  public:
-  bool add_chunk();
+  bool add_chunk(std::string  );
   void start_processing();
   void * get_result();
   Double_buffer();
