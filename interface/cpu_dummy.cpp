117048dea1d65394732dd3518ae2bb36a3bb8367
Daniel <danielowen_b@hotmail.com>
2017-11-16 16:41:51 +0100

    BUG--Can only change the double buffer twice

--- interface/cpu_dummy.cpp
+++ interface/cpu_dummy.cpp
@@ -4,19 +4,24 @@
 #include "double_buffer.hpp"
 
 
-void deploy_chunk(std::string current, Double_buffer *double_buffer){
-  char *chunk_placement_ptr;
+void deploy_chunk(std::string current, Double_buffer *double_buffer, char *chunk_placement_ptr){
   chunk_placement_ptr = double_buffer->get_chunk();
-  strcpy(chunk_placement_ptr, current.c_str());
+  if(chunk_placement_ptr != nullptr){
+
+    std::cout << chunk_placement_ptr << "\n";
+  }
 }
 
 void deploy_work(int total_chunks, int chunks_per_buffer, const std::string *data, Double_buffer *our_double_buffer){
   char *result;
   int counter = 0;
+  char *chunk_placement_ptr;
   while (counter < total_chunks) {
-    for (int i = 0; i < chunks_per_buffer; i++) {
-      deploy_chunk(data[i],our_double_buffer);
+    for (int i = 0; i < chunks_per_buffer ; i++) {
+      std::cout << "The number: " << counter+i << "\n";
+      deploy_chunk(data[i],our_double_buffer, chunk_placement_ptr);
       //counter++;
+      
     }
     our_double_buffer->start_processing();
     std::cout << "start_proc\n" ;
@@ -25,20 +30,44 @@ void deploy_work(int total_chunks, int chunks_per_buffer, const std::string *dat
     counter+= chunks_per_buffer;
   }
 }
+void flipping_buffer(int amount_buffer_flips, int number_of_strings, const std::string *data, Double_buffer *our_double_buffer){
+  char *result;
+  bool flag;  
+  int counter_flips = 0;
+  int counter_chunks = 0;
+  char *chunk_placement_ptr;
+  while (counter_flips < amount_buffer_flips) {
+    flag = true;
+    while (flag) {
+      chunk_placement_ptr = our_double_buffer->get_chunk();
+      if (chunk_placement_ptr == nullptr) {
+        our_double_buffer->start_processing();
+        result = our_double_buffer->get_result();
+        flag = false;
+      }else{
+        strncpy(chunk_placement_ptr, data[counter_chunks % number_of_strings].c_str(), 64);
+      }
+      counter_chunks++;
+    }
+    counter_flips++;
+  }
+}
+
 
 int main(int argc, char *argv[])
 {
   int number_of_strings = 5;
   int chunks_per_buffer = 2;
   Double_buffer *our_double_buffer = new Double_buffer(4);
-  const std::string string_array[number_of_strings] = {"1111111111111111111111111111111111111111111111111111111111111111",
-                                    "2222222222222222222222222222222222222222222222222222222222222222",
-                                    "3333333333333333333333333333333333333333333333333333333333333333",
-                                    "4444444444444444444444444444444444444444444444444444444444444444",
-                                    "5555555555555555555555555555555555555555555555555555555555555555"};
+  const std::string string_array[number_of_strings] = {"111111111111111111111111111111111111111111111111111111111111111",
+                                    "222222222222222222222222222222222222222222222222222222222222222",
+                                    "333333333333333333333333333333333333333333333333333333333333333",
+                                    "444444444444444444444444444444444444444444444444444444444444444",
+                                    "555555555555555555555555555555555555555555555555555555555555555"};
 
-  deploy_work(number_of_strings, chunks_per_buffer, string_array, our_double_buffer);
-
+  //deploy_work(number_of_strings, chunks_per_buffer, string_array, our_double_buffer);
+  flipping_buffer(50, number_of_strings, string_array, our_double_buffer);
+  
   our_double_buffer->done();
 
   return 0;
