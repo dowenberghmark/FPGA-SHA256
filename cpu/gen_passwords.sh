cbbecda7cc02dcf66e264c67ef976406fc5f1ce4
jeffjonasson <jeff.jonasson@gmail.com>
2017-11-17 12:55:55 +0100

    Added condition for input arguments

--- cpu/gen_passwords.sh
+++ cpu/gen_passwords.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 #Generate $1 number of random string that are 20 characters long and outputs into 'rand_pass.txt'
-counter=$1
-until [ $counter -lt 1 ]
-do
-	res=$( cat /dev/random | LC_CTYPE=C tr -dc "[:alpha:]" | head -c 30 )
-	echo $res >> rand_pass.txt
-	((counter--))
-done
-echo All done
+
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./gen_passwords.sh [num of passwords]"
+else
+	counter=$1
+	until [ $counter -lt 1 ]
+	do
+		res=$( cat /dev/random | LC_CTYPE=C tr -dc "[:alpha:]" | head -c 30 )
+		echo $res >> rand_pass.txt
+		((counter--))
+	done
+	echo All done
+fi
