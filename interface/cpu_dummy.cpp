b3fc145b925c5b3516bc105c5c94d26425054267
Daniel <danielowen_b@hotmail.com>
2017-11-20 10:08:22 +0100

    refractored the cpu_dummy into a class

--- interface/cpu_dummy.cpp
+++ interface/cpu_dummy.cpp
@@ -2,24 +2,65 @@
 #include <thread>
 #include <string.h>
 #include "double_buffer.hpp"
+#include <vector>
+class InterfaceTest
+{
+private:
+  int chunks_per_buffer;
+ 
+  Double_buffer *our_double_buffer; //= new Double_buffer(4);
+  std::vector<std::string> string_array;
+  char *chunk_placement_ptr;
+
+  
+  void flipping_buffer(int amount_buffer_flips, int number_of_strings);
+  void deploy_work(int total_chunks, int chunks_per_buffer);
+  void deploy_chunk(std::string current);  
+public:
+  void run_tests();
+  InterfaceTest();
+  ~InterfaceTest();
+};
 
 
-void deploy_chunk(std::string current, Double_buffer *double_buffer, char *chunk_placement_ptr){
-  chunk_placement_ptr = double_buffer->get_chunk();
+InterfaceTest::InterfaceTest(){
+  chunks_per_buffer = 4;
+  our_double_buffer = new Double_buffer(chunks_per_buffer);
+  
+  string_array.push_back("111111111111111111111111111111111111111111111111111111111111111");
+  string_array.push_back("222222222222222222222222222222222222222222222222222222222222222");
+  string_array.push_back("333333333333333333333333333333333333333333333333333333333333333");
+  string_array.push_back("444444444444444444444444444444444444444444444444444444444444444");
+  string_array.push_back("555555555555555555555555555555555555555555555555555555555555555");
+
+
+
+}
+
+InterfaceTest::~InterfaceTest(){
+
+}
+
+void InterfaceTest::run_tests(){
+  this->flipping_buffer(40,string_array.size());
+}
+
+
+void InterfaceTest::deploy_chunk(std::string current){
+  chunk_placement_ptr = our_double_buffer->get_chunk();
   if(chunk_placement_ptr != nullptr){
 
     std::cout << chunk_placement_ptr << "\n";
   }
 }
 
-void deploy_work(int total_chunks, int chunks_per_buffer, const std::string *data, Double_buffer *our_double_buffer){
+void InterfaceTest::deploy_work(int total_chunks, int chunks_per_buffer){
   char *result;
   int counter = 0;
-  char *chunk_placement_ptr;
   while (counter < total_chunks) {
     for (int i = 0; i < chunks_per_buffer ; i++) {
       std::cout << "The number: " << counter+i << "\n";
-      deploy_chunk(data[i],our_double_buffer, chunk_placement_ptr);
+      deploy_chunk(string_array[i]);
       //counter++;
       
     }
@@ -30,12 +71,14 @@ void deploy_work(int total_chunks, int chunks_per_buffer, const std::string *dat
     counter+= chunks_per_buffer;
   }
 }
-void flipping_buffer(int amount_buffer_flips, int number_of_strings, const std::string *data, Double_buffer *our_double_buffer){
+
+void InterfaceTest::flipping_buffer(int amount_buffer_flips, int number_of_strings){
   char *result;
   bool flag;  
   int counter_flips = 0;
   int counter_chunks = 0;
   char *chunk_placement_ptr;
+  
   while (counter_flips < amount_buffer_flips) {
     flag = true;
     while (flag) {
@@ -45,7 +88,7 @@ void flipping_buffer(int amount_buffer_flips, int number_of_strings, const std::
         result = our_double_buffer->get_result();
         flag = false;
       }else{
-        strncpy(chunk_placement_ptr, data[counter_chunks % number_of_strings].c_str(), 64);
+        strncpy(chunk_placement_ptr, string_array[counter_chunks % number_of_strings].c_str(), 64);
       }
       counter_chunks++;
     }
@@ -56,19 +99,8 @@ void flipping_buffer(int amount_buffer_flips, int number_of_strings, const std::
 
 int main(int argc, char *argv[])
 {
-  int number_of_strings = 5;
-  int chunks_per_buffer = 2;
-  Double_buffer *our_double_buffer = new Double_buffer(4);
-  const std::string string_array[number_of_strings] = {"111111111111111111111111111111111111111111111111111111111111111",
-                                    "222222222222222222222222222222222222222222222222222222222222222",
-                                    "333333333333333333333333333333333333333333333333333333333333333",
-                                    "444444444444444444444444444444444444444444444444444444444444444",
-                                    "555555555555555555555555555555555555555555555555555555555555555"};
 
-  //deploy_work(number_of_strings, chunks_per_buffer, string_array, our_double_buffer);
-  flipping_buffer(50, number_of_strings, string_array, our_double_buffer);
-  
-  our_double_buffer->done();
-
+  InterfaceTest tester = InterfaceTest();
+  tester.run_tests();  
   return 0;
 }
