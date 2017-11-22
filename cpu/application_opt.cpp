9f2ae53b868fce0145df92b1fd90eb78e4818b9e
Ishak14 <andy.ishak10@gmail.com>
2017-11-22 15:40:43 +0100

    working application

--- cpu/application_opt.cpp
+++ cpu/application_opt.cpp
@@ -9,73 +9,80 @@
 #include <iomanip>
 #include "sha256.hpp"
 
+
 using namespace std;
 
 #define uchar unsigned char
 #define uint unsigned int
-
-int main() {
+void pre_process(){
 /*ÖPPNAR PASSWORD.TXT*/
   fstream file;
   file.open("password.txt");
   char element[64];
-
   while(!file.eof()){
-
-    memset(element,0x00,64); //fills block memory with 0x00
+    memset(element,0,64); //fills block memory with 0x00
     file >> element;
 
 
 /*appends 1 after string*/
-    unsigned char one = 1 << 7;
-
+    int num = 1;
+    unsigned char one;
+    if(*(char *)&num == 1){   // Little-Endian
+      one = 1 << 7;
+    }
+    else{  // Big-Endian
+      one = 1;
+    }
     int strLen = strlen(element);
     element[strLen++] = one;
     strLen--;
 
 
-/*appendar strlen(element)*8 i binär form */
-    int i=63;
-    int num = 1;
-    
+/*appends strlen(element)*8 to the last byte. Don't think it needs to be big/little endian dependent*/
+    int i=55;
+    unsigned char bytes[4];
+    char l_append;
+    l_append = strLen*8;
     if(*(char *)&num == 1){  // if Little-Endian
-      /*
-      for(int k=0; k<8; k++){
-       element[i] = bitset<64>(strLen*8)[k];
-       printf ("%d: %x \n",k,element[i]);
-       i--;
-       */
-      element[63] = strLen*8;
-     
-     
+      l_append = strLen*8;
+      bytes[0] = (l_append >> 24) & 0xFF;
+      bytes[1] = (l_append >> 16) & 0xFF;
+      bytes[2] = (l_append >> 8) & 0xFF;
+      bytes[3] = l_append & 0xFF;
+      element[60] = bytes[0];
+      element[61] = bytes[1];
+      element[62] = bytes[2];
+      element[63] = bytes[3];    
+    }
+    else{  // Big-Endian
+    }
 
-   }else{  // Big-Endian
-    i = 55;
-    for(int k=0; k<8; k++){
-     element[i] = bitset<8>(strLen*8)[k];
-     printf ("%d: %x \n",k,element[i]);
-     i++;
-   }
- }
-}
+    for(int i=0;i<sizeof(element);i++){
+      printf ("%d: %x \n",i,element[i]);
+    }
 
-for(int i=0;i<sizeof(element);i++){
-  printf ("%d: %x \n",i,element[i]);
-}
+    int counter = 0;
+    for(int i=0;i<64; i++){
+      bitset<8> x(element[i]);
+      if(i == 57){
+        cout << "64 bits from here:" << endl;
+      }
+      cout << x;
+      counter += 8;
+      cout << ", " << endl;
+    }
+    /*vänta på att få skicka chunck*/
+    while(true){
 
-int counter = 0;
-for(int i=0;i<64; i++){
-  bitset<8> x(element[i]);
-  if(i == 57){
-    cout << "64 bits from here:" << endl;
+    }
   }
-  cout << x;
-  counter += 8;
-  cout << ", " << endl;
+
 }
 
+int main() {
 
+pre_process();
 
 //test
-return 0;
+  return 0;
 }
