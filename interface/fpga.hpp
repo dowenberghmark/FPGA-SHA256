28800458e2db40178c6d002b6dd3e8724d847abc
Adam Wendelin <adwe@live.se>
2017-11-22 13:11:20 +0100

    Interface (#1)
    Code for the double buffer implementation in interface. The system can be compiled either for AWS (without fpga dummy) or for local (with fpga dummy) using 'make mode=aws' or 'make mode=local'.
    
    Communication is done using POSIX calls with the file 'dram.hex' in local and the real DRAM on AWS. 
    
    Diagrams explaining the system are added.  
    
    The module runs both locally and on AWS without issue.

---
+++ interface/fpga.hpp
@@ -0,0 +1,40 @@
+#ifndef __FPGA_H_INCLUDED__
+#define __FPGA_H_INCLUDED__
+
+#include <iostream>
+#include <cstdint>
+#include <stdlib.h>
+#include <stdexcept>
+#include <unistd.h> // usleep
+
+#include "defs.hpp"
+
+
+class Fpga {
+ public:
+  Fpga() = default;
+  Fpga(int fd, long int g_header, long int buffer0, long int buffer1);
+  void run();
+  ~Fpga() = default;
+
+ private:
+  struct fpga_buffer {
+    struct buffer_header head;
+    struct chunk data[CHUNKS_PER_BUFFER];
+  };
+
+  struct global_header glob_head;
+  struct buffer_header buf_head;
+  struct fpga_buffer buf;
+  long int dram_bufs[BUFFER_COUNT];
+  long int dram_buf_heads[BUFFER_COUNT];
+  long int dram_glob_head;
+  int dram_fd;
+
+  void read_glob_head();
+  void write_glob_head();
+  void read_buf();
+  void write_result();
+};
+
+#endif
