c8b3e85f21cf23909234d3761a0d387246148200
Cloud User <centos@ip-172-31-40-28.eu-west-1.compute.internal>
2017-11-27 15:03:24 +0000

    structure

--- SDaccel/utils.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __UTILS_H_INCLUDED__
-#define  __UTILS_H_INCLUDED__
-
-int open_file(char const *path);
-int pwrite_all(int fildes, const void *buf, size_t nbyte, off_t offset);
-int pread_all(int fildes, void *buf, size_t nbyte, off_t offset);
-int close_file(int fd);
-
-#endif
