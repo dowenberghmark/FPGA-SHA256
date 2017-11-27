c1748029ea902c6d1f3b54e209c5670b68254a4b
Cloud User <centos@ip-172-31-40-28.eu-west-1.compute.internal>
2017-11-27 14:21:56 +0000

    dd

---
+++ src/cpu_dummy.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <thread>
+#include <string.h>
+#include <vector>
+
+#include "double_buffer.hpp"
+#include "defs.hpp"
+
+
+class InterfaceTest {
+private:
+  DoubleBuffer *our_double_buffer; //= new DoubleBuffer(4);
+  std::vector<std::string> string_array;
+  char *chunk_placement_ptr;
+
+  void test_switch_buffer_n_times(int amount_buffer_switches, int number_of_strings);
+  void deploy_work(int total_chunks, int chunks_per_buffer);
+  void deploy_chunk(std::string current);
+public:
+  void run_tests();
+  InterfaceTest();
+  ~InterfaceTest();
+};
+
+InterfaceTest::InterfaceTest(){
+  char const *dram_path;
+  if (MODE == LOCAL) {
+    dram_path = "dram.hex";
+  } else if (MODE == AWS) {
+    dram_path = "/dev/edma0_queue_0";
+  }
+
+  our_double_buffer = new DoubleBuffer(dram_path);
+
+  string_array.push_back("1111111111111111111111111111111111111111111111111111111111111111");
+  string_array.push_back("2222222222222222222222222222222222222222222222222222222222222222");
+  string_array.push_back("3333333333333333333333333333333333333333333333333333333333333333");
+  string_array.push_back("4444444444444444444444444444444444444444444444444444444444444444");
+  string_array.push_back("5555555555555555555555555555555555555555555555555555555555555555");
+}
+
+InterfaceTest::~InterfaceTest(){
+
+}
+
+void InterfaceTest::run_tests(){
+  this->test_switch_buffer_n_times(10,string_array.size());
+}
+
+void InterfaceTest::deploy_chunk(std::string current){
+  chunk_placement_ptr = our_double_buffer->get_chunk();
+  if(chunk_placement_ptr != nullptr){
+
+    std::cout << chunk_placement_ptr << "\n";
+  }
+}
+
+void InterfaceTest::deploy_work(int total_chunks, int chunks_per_buffer){
+  char *result;
+  int counter = 0;
+  while (counter < total_chunks) {
+    for (int i = 0; i < chunks_per_buffer; i++) {
+      std::cout << "The number: " << counter+i << "\n";
+      deploy_chunk(string_array[i]);
+      //counter++;
+    }
+
+    our_double_buffer->start_processing();
+    std::cout << "start_proc\n" ;
+    result = our_double_buffer->get_result();
+    std::cout << "res\n" ;
+    counter+= chunks_per_buffer;
+  }
+}
+
+void InterfaceTest::test_switch_buffer_n_times(int amount_buffer_switches, int number_of_strings){
+  char *result;
+  bool not_at_end_of_buffer;
+  int counter_flips = 0;
+  int counter_chunks = 0;
+  char *chunk_placement_ptr;
+  while (counter_flips < amount_buffer_switches) {
+    not_at_end_of_buffer = true;
+    while (not_at_end_of_buffer) {
+      chunk_placement_ptr = our_double_buffer->get_chunk();
+      if (chunk_placement_ptr == nullptr) {
+        our_double_buffer->start_processing();
+        result = our_double_buffer->get_result();
+        not_at_end_of_buffer = false;
+      }else{
+        strncpy(chunk_placement_ptr, string_array[counter_chunks % number_of_strings].c_str(), 64);
+        counter_chunks++;
+      }
+    }
+    counter_flips++;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  InterfaceTest tester = InterfaceTest();
+  tester.run_tests();
+  return 0;
+}
---
+++ src/fpga.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+
+#include "fpga.hpp"
+#include "utils.hpp"
+
+
+Fpga::Fpga(int fd, long int glob_head, long int buf0, long int buf1) {
+  // set dram addresses.
+  dram_fd = fd;
+  dram_buf_heads[0] = buf0;
+  dram_buf_heads[1] = buf1;
+  dram_bufs[0] = dram_buf_heads[0] + BUFFER_HEADER_SIZE;
+  dram_bufs[1] = dram_buf_heads[1] + BUFFER_HEADER_SIZE;
+  dram_glob_head = glob_head;
+}
+
+// run this function as a thread. communicate through the shared file.
+void Fpga::run() {
+  // dummy fpga loop
+  while (1) {
+    do {
+      read_glob_head();
+      usleep(5000);
+    } while (!glob_head.start_proc);
+
+    usleep(500000);
+    // only for testing so we can quit nicely
+    if (glob_head.start_proc == 1337) {
+      break;
+    }
+    // restore start proc flag.
+    glob_head.start_proc = 0;
+    write_glob_head();
+
+    read_buf();
+    // do dummy sha-256 to received chunks
+    struct chunk *chunks = buf.data;
+    for (size_t i = 0; i < buf.head.num_chunks; i++) {
+      for (int j = 0; j < 32; j++) {
+	chunks[i].data[j] = i;
+      }
+      std::cout << "result written to chunk: " << i << "\n";
+    }
+    std::cout << "buffer" << glob_head.active_buf << " processed, num_chunks: " << buf.head.num_chunks << " rdy_flag: " << buf.head.rdy_flag << "\n";
+
+    // write back result and ready flag
+    buf.head.rdy_flag = 1;
+    write_result();
+  }
+  std::cout << "quitting fpga run" << "\n";
+}
+
+void Fpga::read_glob_head() {
+  int ret;
+  ret = pread_all(dram_fd, &glob_head, GLOBAL_HEADER_SIZE, dram_glob_head);
+  if (ret < 0) {
+    throw std::runtime_error("Can't read from dram to fpga.");
+  }
+}
+
+void Fpga::write_glob_head() {
+  int ret;
+  ret = pwrite_all(dram_fd, &glob_head, GLOBAL_HEADER_SIZE, dram_glob_head);
+  if (ret < 0) {
+    throw std::runtime_error("Can't write to dram from fpga.");
+  }
+}
+
+void Fpga::read_buf() {
+  int ret;
+  ret = pread_all(dram_fd, &buf.head, BUFFER_HEADER_SIZE, dram_buf_heads[glob_head.active_buf]);
+  if (ret < -1) {
+    throw std::runtime_error("Can't read from dram to fpga.");
+  }
+  // only read the chunks that have been written to.
+  ret = pread_all(dram_fd, &buf.data, CHUNK_SIZE*buf.head.num_chunks, dram_bufs[glob_head.active_buf]);
+  if (ret < -1) {
+    throw std::runtime_error("Can't read from dram to fpga.");
+  }
+}
+
+void Fpga::write_result() {
+  int ret;
+  ret = pwrite_all(dram_fd, &buf.head, BUFFER_HEADER_SIZE+CHUNK_SIZE*buf.head.num_chunks, dram_buf_heads[glob_head.active_buf]);
+  if (ret < -1) {
+    throw std::runtime_error("Can't write to dram from fpga.");
+  }
+}
