48933c515eec55e11ab665452efae381dc30e3a9
Snapple49 <oliver-dot-stein@hotmail.com>
2017-11-29 11:08:03 +0100

    made some changes in sha256.c, sha.c, still bugsearching

--- shatest/sha.c
+++ shatest/sha.c
@@ -1,15 +1,20 @@
-#include<stdio.h>
-#include<stdint.h>
+#include <stdio.h>
+#include <stdint.h>
 #include "sha256.h"
 #include "sha_preprocess.hpp"
 
 main(){
-    char* test = "hej";
-    printf("%s\n", "1");
-    pre_process(test);
-    printf("%s\n", "1");
-   	sha256((uint32_t*)test);
+  char test[64] = {'\0'};
+  test[0]= 'a'; //hash of "a" is
+  //ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb
+  //14ffffffd660ffffffa16115ffffffa1ffffffd1
+  //printf("%s\n", "1");
+  pre_process(test);
+  //printf("%s\n", "1");
+  sha256((uint32_t*)test);
 
-    printf((char*)test);
-
+  for (int i = 0; i < 8; i+=sizeof(uint32_t)){
+    printf("%x\n", (uint32_t)test[i]);
+  }
+  
 }
