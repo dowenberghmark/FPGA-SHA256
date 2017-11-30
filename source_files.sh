dfdaac2620fbd9ce69bbab2e576e18fc39facc02
Adam Wendelin <adwe@live.se>
2017-11-30 15:42:56 +0100

    removed old files that are not used

--- source_files.sh
+++ source_files.sh
@@ -5,4 +5,11 @@ source hdk_setup.sh
 source sdaccel_setup.sh
 source /opt/Xilinx/SDx/2017.1.op/settings64.sh
 cd $p
-source vendor_exports.sh
+
+export CLOCK_A_RECIPE=0
+export CLOCK_B_RECIPE=0
+export CLOCK_C_RECIPE=0
+export device_id=0xF001
+export vendor_id=0x1D0F
+export subsystem_id=0x1D51
+export subsystem_vendor_id=0xFEDC
