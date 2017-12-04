03ff7aeda0fc81a9f129aff789d0a1f59cee8be2
Ishak14 <andy.ishak10@gmail.com>
2017-12-04 18:00:59 +0100

    fixed feedback from saraback & fixed bug in sha_preprocess

--- cpu/sha_preprocess.cpp
+++ cpu/sha_preprocess.cpp
@@ -3,40 +3,23 @@
 #include <string>
 #include <sstream>
 #include <bitset>
-#include <string.h>
-
-using namespace std;
 
 #define uchar unsigned char
 #define uint unsigned int
 
 void pre_process(char *element){
   /*Initilaize*/
-  int num = 1;
-  int strLen = strlen(element);
-  uint64_t l_append;
-  uchar one;
-  /*appends 1 after string*/
+  int str_len = strlen(element);
+  uint64_t element_length_append;
+  element_length_append = str_len*8;
+  element[str_len++] = 0x80;
 
-    if(*(char *)&num == 1) {   // Little-Endian
-      one = 0x80;
-    } else {  // Big-Endian
-      one = 1;
-    }
-    
-    element[strLen++] = one;
-    strLen--;
-    l_append = strLen*8;
-
-    if(*(char *)&num == 1) {  // if Little-Endian
-      l_append = strLen*8;
-      element[56] = (l_append >> 56) & 0xFF;
-      element[57] = (l_append >> 48) & 0xFF;
-      element[58] = (l_append >> 40) & 0xFF;
-      element[59] = (l_append >> 32) & 0xFF;
-      element[60] = (l_append >> 24) & 0xFF;
-      element[61] = (l_append >> 16) & 0xFF;
-      element[62] = (l_append >> 8) & 0xFF;
-      element[63] = l_append & 0xFF;  
-    } else{} // Big-Endian
-  }
+  element[56] = (element_length_append >> 56) & 0xFF;
+  element[57] = (element_length_append >> 48) & 0xFF;
+  element[58] = (element_length_append >> 40) & 0xFF;
+  element[59] = (element_length_append >> 32) & 0xFF;
+  element[60] = (element_length_append >> 24) & 0xFF;
+  element[61] = (element_length_append >> 16) & 0xFF;
+  element[62] = (element_length_append >> 8) & 0xFF;
+  element[63] = element_length_append & 0xFF;  
+}
