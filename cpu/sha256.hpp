0048f3abf73a9732e38fa48cbf6e7bf71da8755b
Ishak14 <andy.ishak10@gmail.com>
2017-12-05 13:20:00 +0100

    added cpu and main files to merge branch

---
+++ cpu/sha256.hpp
@@ -0,0 +1,8 @@
+#ifndef __SHA256_H_INCLUDED__
+#define __SHA256_H_INCLUDED__
+
+#include <string.h>
+ int SHA256_CPU_debug(std::string filename);
+ std::vector<std::string> SHA256_CPU_verify(std::string filename);
+
+ #endif
