009eee97f2e25a13c12435e842d10e9bf4882ac2
jeffjonasson <jeff.jonasson@gmail.com>
2017-12-11 16:36:11 +0100

    Fixes

--- cpu/verify.cpp
+++ cpu/verify.cpp
@@ -17,15 +17,15 @@ int verify(std::vector<std::string> fpga_hash_vec){
   cout << "Trying to verify" << endl;
   vector<string>ext_hash_vec = SHA256_CPU_verify("cpu/random_passwords.txt");
   int error_row = 0;
+  int errors = 0;
 
   for(unsigned i=0; i<fpga_hash_vec.size(); i++){
-    cout << "FPGA hash:" << i << " " << fpga_hash_vec[i] << endl;
-    cout << "EXT  hash:" << i << " " << ext_hash_vec[i] << endl;
     if (fpga_hash_vec[i].compare(ext_hash_vec[i]) != 0){
       error_row = i+1;
       cout << "Line " << error_row << " is incorrect" << endl;
+      errors++;
     }
   }
-  cout << "Comparison completed" << endl;
+  cout << "Comparison completed. " << errors << "nr of errors." << endl;
   return 0;
 }
