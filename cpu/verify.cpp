2d0a8c660c9e29e4fbde47b56f56ec77eeca0bdc
jeffjonasson <jeff.jonasson@gmail.com>
2017-12-13 13:47:30 +0100

    Added changes to verify to it takes in a vector instead of a file path

--- cpu/verify.cpp
+++ cpu/verify.cpp
@@ -12,21 +12,20 @@ using std::cout;
 using std::vector;
 using std::ifstream;
 
-int verify(std::string filename){
+int verify(std::vector<std::string> fpga_hash_vec){
   //Get vector with hashed passwords from an extrernal SHA-256 hasher
-  vector<string>pass_vec = SHA256_CPU_verify("random_passwords.txt");
+  cout << "Trying to verify" << endl;
+  vector<string>ext_hash_vec = SHA256_CPU_verify("cpu/random_passwords.txt");
+  int error_row = 0;
+  int errors = 0;
 
-  ifstream file;
-  file.open(filename);
-  string element;
-  int counter = 0;
-  while(!file.eof()){
-    file >> element;
-    if (element.compare(pass_vec[counter]) != 0){
-      cout << "Line " << counter+1 << " is incorrect" << endl;
+  for(unsigned i=0; i<fpga_hash_vec.size(); i++){
+    if (fpga_hash_vec[i].compare(ext_hash_vec[i]) != 0){
+      error_row = i+1;
+      cout << "Line " << error_row << " is incorrect" << endl;
+      errors++;
     }
-    counter++;
   }
-  cout << "Comparison completed" << endl;
+  cout << "Comparison completed. " << errors << " errors." << endl;
   return 0;
 }
