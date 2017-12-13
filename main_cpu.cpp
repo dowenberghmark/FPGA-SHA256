bf157ffbbbe36d36e1b8f63b6e3870e2b1cd6435
Cloud User <centos@ip-172-31-34-178.eu-west-1.compute.internal>
2017-12-13 12:53:50 +0000

    compiling but gives wrong

--- main_cpu.cpp
+++ main_cpu.cpp
@@ -218,7 +218,7 @@ int main(int argc, char ** argv) {
 
   if (vopt) {
     std::cout << "====================== VERIFICATION RESULTS =======================" << std::endl;
-    verify(filename);
+    
     std::cout << "================================================================" << std::endl;
   }
   return 0;
