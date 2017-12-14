511ef5ff069a5c7f0b6b21aa3b5ba0d05a66140d
Cloud User <centos@ip-172-31-45-38.eu-west-1.compute.internal>
2017-12-14 12:41:55 +0000

    Tested with cpubenchmark working

--- benchmark.sh
+++ benchmark.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 F=$1 #  Name of path/output file
 PASS=$2 # Path to password file
-PROGRAM=sha256 # Not in use at the moment
+PROGRAM="./sha256 -b" # Not in use at the moment
 SIZE=(0.1 1 10 100 1000 10000 100000 1000000 10000000 100000000)
 N=10
-
-if [ $3 == 'cpu']
+CPU='cpu'
+if [ $3 = $CPU ]
 then
-    PROGRAM=host_sha256
+    PROGRAM="./host/host_sha256"
 fi
 
 echo "Starting benchmarks for $((N)) iterations."
@@ -15,6 +15,7 @@ echo "Starting benchmarks for $((N)) iterations."
 for  s in ${SIZE[*]}; do 		
     for ((i=1;i<$((N+1));i++));	do 			
 	echo "Iteration: $i, Size: $s"
-	eval ./$PROGRAM -b -s $s -f $PASS -o $F
+	eval $PROGRAM  -s $s -f $PASS -o $F
+	echo "$PROGRAM  -s $s -f $PASS -o $F"
     done
 done
