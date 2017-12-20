52c69f2aa8b24e93bfacdd49f10ee4ff56130f3c
callebsa <carl.bostrom.9402@gmail.com>
2017-12-20 14:35:01 +0100

    fixed review

--- charts/charts.py
+++ charts/charts.py
@@ -5,19 +5,19 @@ from matplotlib import pyplot as plt
 import matplotlib
 
 
-def plotjpg2(ss, out, header):
+def plot(data, filename, title):
     color = ['bD', 'go', 'rh', 'cx', 'mp', 'y<', 'k', 'w']
-    for x in xrange(1, len(ss)):
-        for c in xrange(1, len(ss[x])):
-            ss[x][c] = ss[x][c]/ss[0][c]
-        plt.plot(ss[0][1:], ss[x][1:], color[x-1]+"-", label=ss[x][0][:-4])
+    for x in xrange(1, len(data)):
+        for c in xrange(1, len(data[x])):
+            data[x][c] = data[x][c]/data[0][c]
+        plt.plot(data[0][1:], data[x][1:], color[x-1]+"-", label=data[x][0][:-4])
     plt.gca().set_xscale('log')
     plt.legend(loc="upper right")
     plt.xlabel('Amount of hashed data (MB)')
     plt.ylabel('Throughput (MB/s)')
-    plt.title(header)
+    plt.title(title)
     plt.grid(True)
-    plt.savefig(out+".pdf")
+    plt.savefig(filename+".pdf")
 
 
 if __name__ == '__main__':
@@ -25,14 +25,14 @@ if __name__ == '__main__':
     argument_parser.add_argument('--folder',
                                  help="folder with csv files",
                                  default="")
-    argument_parser.add_argument('--header',
-                                 help="The header of the graph",
+    argument_parser.add_argument('--title',
+                                 help="The title of the graph",
                                  default="A comparison between hashing on FPGA and CPU")
-    argument_parser.add_argument('--out',
+    argument_parser.add_argument('--filename',
                                  help="The filename of the graph, .pdf will be added",
                                  default="chartsGraph")
-    argument_parser.add_argument('--mesure',
-                                 help="mesure per size",
+    argument_parser.add_argument('--MPP',
+                                 help="measurements per point",
                                  default=10,
                                  type=int)
 
@@ -40,36 +40,33 @@ if __name__ == '__main__':
     if len(args.folder) == 0:
         print "No input folder set, use --folder "
         exit()
-    lst = [["Datas"]]
+    lst = [["Data sizes"]]
     for (dirpath, dirnames, filenames) in os.walk(os.getcwd()+"/"+args.folder):
         for filename in filenames:
             if ".csv" in filename:
                 lst.append([filename])
     first = True
-    skip = False
     for x in xrange(1, len(lst)):
-        mesures = args.mesure-1
+        measuresCount = args.MPP-1
         sum = 0
-        nn = 0
         for line in open(os.getcwd() + "/" + args.folder + "/" + lst[x][0]):
-            nn += 1
             if line[-1] == "\n":
                 line = line[:-1]
             if line[-1] == "\r":
                 line = line[:-1]
             linesplit = line.split(";")
-            if first and mesures == args.mesure-1:
+            if first and measuresCount == args.MPP-1:
                 if linesplit[0] == '0':
                     skip = True
                 else:
                     lst[0].append(float(linesplit[0]))
                 sum += float(linesplit[1])
-            elif mesures == 0:
-                lst[x].append(sum/args.mesure)
+            elif measuresCount == 0:
+                lst[x].append(sum/args.MPP)
                 sum = 0
-                mesures = args.mesure
+                measuresCount = args.MPP
             else:
                 sum += float(linesplit[1])
-            mesures -= 1
+            measuresCount -= 1
         first = False
-    plotjpg2(lst, args.out, args.header)
+    plot(lst, args.filename, args.title)
