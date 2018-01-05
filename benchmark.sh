6addd0ea9d3848aecd9fabd26be8fa58309d271d
Adam Wendelin <adwe@live.se>
2018-01-05 10:38:22 +0100

    reduced benchmarks (#36)

--- benchmark.sh
+++ benchmark.sh
@@ -6,9 +6,9 @@ then
 fi
 F=$1 #  Name of path/output file
 PASS=$2 # Path to password file
-PROGRAM="./sha256 -b" # Not in use at the moment
-SIZE=(0.001 0.01 0.1 1 10 100 1000 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000)
-N=10
+PROGRAM="./sha256 -B 1000 -b"
+SIZE=(0.001 0.01 0.1 1 10 100 1000 10000 100000)
+N=5
 CPU='host'
 
 if [ -f "$F" ]
@@ -22,9 +22,9 @@ then
 fi
 
 echo "Starting benchmarks for $((N)) iterations."
-# Start 
-for  s in ${SIZE[*]}; do 		
-    for ((i=1;i<$((N+1));i++));	do 			
+# Start
+for  s in ${SIZE[*]}; do
+    for ((i=1;i<$((N+1));i++));	do
 	echo "Iteration: $i, Size: $s"
 	eval $PROGRAM  -s $s -f $PASS -o $F
     done
